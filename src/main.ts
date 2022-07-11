af787158c5bec1ef0830b711c744836978460786
wunlei <confusioncosmos@gmail.com>
2022-07-11 18:13:11 +0300

    feat: add default port value, add .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { DEFAULT_PORT } from './constants/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT || DEFAULT_PORT);
 }
 bootstrap();
